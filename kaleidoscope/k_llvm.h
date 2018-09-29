8b3baf840f84d033e2bbe7a0a33533b35f311dca
Cristiano Pereira <cristiano.l.pereira@gmail.com>
2018-09-29 13:42:52 -0700

    added llvm includes into a single file

---
+++ kaleidoscope/k_llvm.h
@@ -0,0 +1,16 @@
+#ifndef _K_LLVM_H_
+#define _K_LLVM_H_
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+#endif
