885c448a0fa092efd29edca5edbbdc415111578f
Cristiano Pereira <cristiano.l.pereira@gmail.com>
2019-01-08 10:09:45 -0800

    added missingexterns.h

---
+++ kaleidoscope/externs.h
@@ -0,0 +1,6 @@
+#include <math.h>
+
+/// putchard - putchar that takes a double and returns 0.
+extern "C" double putchard(double X);
+/// printd - printf that takes a double prints it as "%f\n", returning 0.
+extern "C" double printd(double X);
